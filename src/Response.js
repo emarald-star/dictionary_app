3d0b93f1f6fc4d6a8ff1b5b3b8833361df4f6160
emarald-star <Oxford@01234>
2022-05-02 14:39:03 -0700

    Add meanings

--- src/Response.js
+++ src/Response.js
@@ -1,22 +1,40 @@
 import React from "react";
 import "./Response.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 
 export default function Response(props){
-    
+    let meanings = props.results.meanings
     console.log(props.results)
     return(
+
         <div className="Response">
             <h2 className="Main-word">{props.results.word}   
-            <FontAwesomeIcon icon="fa-solid fa-volume" />
+            {/* <FontAwesomeIcon icon="fa-solid fa-volume" /> */}
                 
             </h2>
-            <h3>
-                {props.results.meanings[0].definitions[0].definition}
-            </h3>
+            <div>
+                {props.results.phonetics.map((phonetic,index)=>(
+                <h4 className="Phonetic" key={index}> {phonetic.text}
+                </h4>))}
+                
+                {meanings.map((meaning,index)=> (
+                    <div className="PartsOfSpeech" key={index}>
+                    {meaning.partOfSpeech}
+
+                        {meaning.definitions.map((definition,index) => (
+                            <h5 className="Definition" key={index}>
+                                {definition.definition}
+                                <div className="Example">
+                                    {definition.example}
+                                </div>
+                            </h5>
+                        ))}
+                    </div>
+                ) )}
+            </div>
         </div>
     )
 }
